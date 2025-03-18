b7cfae117c99249153d2160f0b068d17cf958801
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-18 16:35:39 +0000

    Add function to generate a grid of squares.

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const maxGridSize = 16;
-const gridSquareSizeInPx = 5;
+const gridSquareSize = 5;
+const gridSquareBorderSize = 1;
 
 const mainApp = document.querySelector("#root");
 
 const mainGrid = document.createElement("div");
 mainGrid.setAttribute("id", "main-grid");
+mainGrid.style.width = maxGridSize * (gridSquareSize + gridSquareBorderSize * 2) + "px";
 
 mainApp.appendChild(mainGrid);
 
 
 const singleSquareGrid = document.createElement("div");
-singleSquareGrid.style.width = gridSquareSizeInPx + "px";
-singleSquareGrid.style.height = gridSquareSizeInPx + "px";
-singleSquareGrid.style.border = "1px solid grey";
+singleSquareGrid.style.width = gridSquareSize + "px";
+singleSquareGrid.style.height = gridSquareSize + "px";
+singleSquareGrid.style.border = "" + gridSquareBorderSize + "px solid grey"
+singleSquareGrid.setAttribute("class", "singleGridSquare");
 
 
-for(let i = 0; i < maxGridSize; i++){
+for(let i = 0; i < maxGridSize ** 2; i++){
     let newElement = singleSquareGrid.cloneNode();//if ID is used, modify it before appending the child to parent!
     mainGrid.appendChild(newElement);
 };
