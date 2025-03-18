ed41d89aba96cd0312c70f1e52218db5687d82dd
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-18 16:20:08 +0000

    Add basic html/css template and generic grid template generator.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("JS loaded");
+const maxGridSize = 16;
+const gridSquareSizeInPx = 5;
+
+const mainApp = document.querySelector("#root");
+
+const mainGrid = document.createElement("div");
+mainGrid.setAttribute("id", "main-grid");
+
+mainApp.appendChild(mainGrid);
+
+
+const singleSquareGrid = document.createElement("div");
+singleSquareGrid.style.width = gridSquareSizeInPx + "px";
+singleSquareGrid.style.height = gridSquareSizeInPx + "px";
+singleSquareGrid.style.border = "1px solid grey";
+
+
+for(let i = 0; i < maxGridSize; i++){
+    let newElement = singleSquareGrid.cloneNode();//if ID is used, modify it before appending the child to parent!
+    mainGrid.appendChild(newElement);
+};
