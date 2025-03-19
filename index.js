d6df95e2941d00f2f8671691e543d14c40fe2f28
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-19 11:56:26 +0000

    Add button to select grid size.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const drawingBoardSize = 100;
-let numOfPixelsPerSide = 8;
+let numOfPixelsPerSide = 8; //starting default pixel size
 const gridSquareBorderSize = 1;
 let gridPixelSize = getGridPixelSize(drawingBoardSize, numOfPixelsPerSide, gridSquareBorderSize);
 const mainApp = document.querySelector("#root");
+
+const btnSetNumOfPixels = document.createElement("button");
+btnSetNumOfPixels.textContent = "Set grid density.";
+btnSetNumOfPixels.addEventListener("click", ()=>{
+    numOfPixelsPerSide = prompt("Select number of pixels per side (max size 100)",);
+    if(numOfPixelsPerSide > 100){ numOfPixelsPerSide = 100;};
+    createNewDrawingBoard();
+});
+
+mainApp.appendChild(btnSetNumOfPixels);
 
 const mainGrid = document.createElement("div");
 mainGrid.setAttribute("id", "main-grid");
@@ -16,6 +26,17 @@ const singleSquareGrid = document.createElement("div");
 
 function createNewDrawingBoard(){
 
+    /*Remove existing board if present*/
+    let existingBoardNodeList = document.querySelectorAll("#main-grid>div");
+    if(existingBoardNodeList.length > 0){
+        existingBoardNodeList.forEach((element)=>{
+            element.remove();
+        });
+    };
+
+    /*set new board dimensions*/
+    gridPixelSize = getGridPixelSize(drawingBoardSize, numOfPixelsPerSide, gridSquareBorderSize);
+
     singleSquareGrid.style.width = gridPixelSize + "px";
     singleSquareGrid.style.height = gridPixelSize + "px";
     singleSquareGrid.style.border = gridSquareBorderSize + "px solid grey"
@@ -25,6 +46,7 @@ function createNewDrawingBoard(){
         let newElement = singleSquareGrid.cloneNode();//if any unique attributes are used (like ID), modify them before appending the child to parent!
         newElement.addEventListener("mouseover",(e)=>{gridMouseOver(e)});
         newElement.addEventListener("mouseout",(e)=>{gridMouseOut(e)});
+        newElement.setAttribute("id","grid-pixel-" + (i + 1));
         mainGrid.appendChild(newElement);
     };
 };
