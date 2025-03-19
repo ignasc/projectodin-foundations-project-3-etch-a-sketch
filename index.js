2df25d67bd192983290c7a302f20e7c03709c4cb
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-19 10:19:51 +0000

    Make squares change color on mouseover.

--- index.js
+++ index.js
@@ -17,8 +17,16 @@ singleSquareGrid.style.height = gridSquareSize + "px";
 singleSquareGrid.style.border = "" + gridSquareBorderSize + "px solid grey"
 singleSquareGrid.setAttribute("class", "singleGridSquare");
 
+function gridMouseOver(e){
+    e.target.style.backgroundColor = "grey";
+};
+function gridMouseOut(e){
+    e.target.style.backgroundColor = "white";
+};
 
 for(let i = 0; i < maxGridSize ** 2; i++){
     let newElement = singleSquareGrid.cloneNode();//if any unique attributes are used (like ID), modify them before appending the child to parent!
+    newElement.addEventListener("mouseover",(e)=>{gridMouseOver(e)});
+    newElement.addEventListener("mouseout",(e)=>{gridMouseOut(e)});
     mainGrid.appendChild(newElement);
 };
